215ad5a9371e308945bc72ea53bed388b2c7419d
williamorim <thewilliam89@gmail.com>
2025-02-26 17:19:32 -0300

    graficos secao 1.1

--- R/graficos.R
+++ R/graficos.R
@@ -1,10 +1,9 @@
-#' Geração de Energia Elétrica: fontes não-renováveis
-#' 
-#' Capítulo I
-#' 
+#' Gráfico do Capítulo I
+#'
 #' @param con Conexão com o banco de dados
-#' 
-#' @export 
+#' @param lang Idioma
+#'
+#' @export
 grafico_geracao_energia_fontes_nr <- function(con, lang = "pt") {
   tab <- dplyr::tbl(con, "grafico_geracao_energia_fontes_nr") |>
     dplyr::collect() |>
@@ -56,4 +55,201 @@ grafico_geracao_energia_fontes_nr <- function(con, lang = "pt") {
     ggplot2::geom_vline(xintercept = min(tab$ano), color = "black", linetype = "solid", size = 0.5) +
     ggplot2::geom_hline(yintercept = 0, color = "black", linetype = "solid", size = 0.35) +
     ggplot2::ylab("GWh")
+}
+
+#' Gráfico do Capítulo I
+#'
+#' @param con Conexão com o banco de dados
+#' @param lang Idioma
+#'
+#' @export
+grafico_oferta_interna_el_fonte <- function(con, lang = "pt") {
+  tab <- dplyr::tbl(con, "grafico_oferta_interna_el_fonte") |>
+    dplyr::collect() |>
+    dplyr::mutate(
+      cor = pegar_cor(variavel)
+    )
+
+  if (lang != "pt") {
+    tab$variavel <- tab[[glue::glue("variavel_{lang}")]]
+  }
+
+  cores <- setNames(tab$cor, tab$variavel)
+
+  tab |>
+    ggplot2::ggplot(ggplot2::aes(x = variavel, y = valores, fill = variavel)) +
+    ggplot2::geom_bar(stat = "identity", color = "#404040", size = 0.25, width = 1) +
+    ggplot2::geom_text(ggplot2::aes(label = scales::percent(valores, accuracy = 0.1)),
+      vjust = 0.5, hjust = -0.25, size = 2.5, fontface = "bold"
+    ) +
+    ggplot2::geom_segment(ggplot2::aes(x = as.numeric(variavel) - 0.5, xend = as.numeric(variavel) + 0.5, y = 0, yend = 0), size = 0.6) +
+    ggplot2::coord_flip() +
+    ggplot2::scale_fill_manual(values = cores) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "none",
+      axis.title.y = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 6.5)
+    ) +
+    ggplot2::scale_y_continuous(limits = c(0, 0.75))
+}
+
+#' Gráfico do Capítulo I
+#'
+#' @param con Conexão com o banco de dados
+#' @param lang Idioma
+#'
+#' @export
+grafico_variacao_consumo_setorial_el <- function(con, lang = "pt") {
+  tab <- dplyr::tbl(con, "grafico_variacao_consumo_setorial_el") |>
+    dplyr::collect()
+
+  if (lang != "pt") {
+    tab$variavel <- tab[[glue::glue("variavel_{lang}")]]
+  }
+
+  tab |>
+    ggplot2::ggplot(ggplot2::aes(x = variavel, y = valores, fill = variavel)) +
+    ggplot2::geom_bar(stat = "identity", color = "#404040", size = 0.25, width = 0.8) +
+    ggplot2::scale_fill_manual(values = ifelse(tab$valores > 0, "#00678e", "#953735")) +
+    ggplot2::geom_text(ggplot2::aes(label = scales::percent(valores, accuracy = 0.1)),
+      vjust = ifelse(tab$valores >= 0, 0.25, 0.25),
+      hjust = ifelse(tab$valores >= 0, -0.25, 1.2), size = 2.5, fontface = "bold"
+    ) +
+    ggplot2::geom_segment(ggplot2::aes(x = as.numeric(variavel) - 0.5, xend = as.numeric(variavel) + 0.5, y = 0, yend = 0), size = 0.6) +
+    ggplot2::coord_flip() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "none",
+      axis.text.x = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank(),
+      axis.title.y = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 6.5)
+    ) +
+    ggplot2::scale_y_continuous(limits = c(-0.10, 0.10))
+}
+
+#' Gráfico do Capítulo I
+#'
+#' @param con Conexão com o banco de dados
+#' @param lang Idioma
+#'
+#' @export
+grafico_crescimento_consumo_eletricidade_industria <- function(con, lang = "pt") {
+  tab <- dplyr::tbl(con, "grafico_crescimento_consumo_eletricidade_industria") |>
+    dplyr::collect()
+
+  if (lang != "pt") {
+    tab$variavel <- tab[[glue::glue("variavel_{lang}")]]
+  }
+
+  tab |>
+    ggplot2::ggplot(ggplot2::aes(x = variavel, y = valores, fill = variavel)) +
+    ggplot2::geom_bar(stat = "identity", color = "#404040", size = 0.25, width = 0.9) +
+    ggplot2::scale_fill_manual(values = ifelse(tab$valores > 0, "#00678e", "#953735")) +
+    ggplot2::geom_text(ggplot2::aes(label = scales::percent(valores, accuracy = 0.1)),
+      vjust = ifelse(tab$valores >= 0, 0.25, 0.25),
+      hjust = ifelse(tab$valores >= 0, -0.25, 1.2), size = 2.5, fontface = "bold"
+    ) +
+    ggplot2::geom_segment(ggplot2::aes(x = as.numeric(variavel) - 0.5, xend = as.numeric(variavel) + 0.5, y = 0, yend = 0), size = 0.6) +
+    ggplot2::coord_flip() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 6.5),
+      axis.title.y = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank()
+    ) +
+    ggplot2::scale_y_continuous(limits = c(-0.10, 0.15))
+}
+
+#' Gráfico do Capítulo I
+#'
+#' @param con Conexão com o banco de dados
+#' @param lang Idioma
+#'
+#' @export
+grafico_participacao_setorial_consumo_el <- function(con, lang = "pt") {
+  tab <- dplyr::tbl(con, "grafico_participacao_setorial_consumo_el") |>
+    dplyr::collect() |>
+    dplyr::mutate(
+      cor = pegar_cor(variavel)
+    )
+
+  if (lang != "pt") {
+    tab$variavel <- tab[[glue::glue("variavel_{lang}")]]
+  }
+
+  cores <- setNames(tab$cor, tab$variavel)
+
+  tab |>
+    ggplot2::ggplot(ggplot2::aes(x = variavel, y = valores, fill = variavel)) +
+    ggplot2::geom_bar(stat = "identity", color = "#404040", size = 0.25, width = 1) +
+    ggplot2::geom_text(ggplot2::aes(label = scales::percent(valores, accuracy = 0.1)),
+      vjust = 0.5, hjust = -0.25, size = 2.5, fontface = "bold"
+    ) +
+    ggplot2::geom_segment(ggplot2::aes(x = as.numeric(variavel) - 0.5, xend = as.numeric(variavel) + 0.5, y = 0, yend = 0), size = 0.6) +
+    ggplot2::coord_flip() +
+    ggplot2::scale_fill_manual(values = cores) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "none",
+      axis.title.y = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 6.5)
+    ) +
+    ggplot2::scale_y_continuous(limits = c(0, 0.45))
+}
+
+#' Gráfico do Capítulo I
+#'
+#' @param con Conexão com o banco de dados
+#' @param lang Idioma
+#'
+#' @export
+grafico_participacao_fontes_capacidade_instalada <- function(con, lang = "pt") {
+  tab <- dplyr::tbl(con, "grafico_participacao_fontes_capacidade_instalada") |>
+    dplyr::collect() |>
+    dplyr::mutate(
+      cor = pegar_cor(variavel)
+    )
+
+  if (lang != "pt") {
+    tab$variavel <- tab[[glue::glue("variavel_{lang}")]]
+  }
+
+  cores <- setNames(tab$cor, tab$variavel)
+
+  tab |>
+    ggplot2::ggplot(ggplot2::aes(x = variavel, y = valores, fill = variavel)) +
+    ggplot2::geom_bar(stat = "identity", color = "#404040", size = 0.25, width = 1) +
+    ggplot2::geom_text(ggplot2::aes(label = scales::percent(valores, accuracy = 0.1)),
+      vjust = 0.5, hjust = -0.25, size = 2.5, fontface = "bold"
+    ) +
+    ggplot2::geom_segment(ggplot2::aes(x = as.numeric(variavel) - 0.5, xend = as.numeric(variavel) + 0.5, y = 0, yend = 0), size = 0.6) +
+    ggplot2::coord_flip() +
+    ggplot2::scale_fill_manual(values = cores) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "none",
+      axis.title.y = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 6.5)
+    ) +
+    ggplot2::scale_y_continuous(limits = c(0, 0.65))
 }
--- inst/scripts/gerar_banco_ouro.R
+++ inst/scripts/gerar_banco_ouro.R
@@ -1,7 +1,45 @@
 con <- DBI::dbConnect(duckdb::duckdb(), "data-raw/bd_ouro.duckdb")
 
+# grafico_geracao_energia_fontes_nr
 tab <- readr::read_rds("data-raw/rds/pt/dados_agregados_1_1_a_tratados.rds")
-tab_en <- readr::read_rds("data-raw/rds/en/dados_agregados_1_1_a_tratados.rds") |> 
+tab_en <- readr::read_rds("data-raw/rds/en/dados_agregados_1_1_a_tratados.rds") |>
+  dplyr::select(variavel_en = variavel)
+DBI::dbWriteTable(con, "grafico_geracao_energia_fontes_nr", tab, overwrite = TRUE)
+
+# grafico_oferta_interna_el_fonte
+tab <- readr::read_rds("./data-raw/rds/pt/dados_agregados_1_1_b_tratados.rds")
+tab_en <- readr::read_rds("./data-raw/rds/en/dados_agregados_1_1_b_tratados.rds") |>
   dplyr::select(variavel_en = variavel)
 tab <- cbind(tab, tab_en)
-DBI::dbWriteTable(con, "grafico_geracao_energia_fontes_nr", tab, overwrite = TRUE)
+DBI::dbWriteTable(con, "grafico_oferta_interna_el_fonte", tab, overwrite = TRUE)
+
+# grafico_variacao_consumo_setorial_el
+tab <- readr::read_rds("./data-raw/rds/pt/dados_agregados_1_1_1_tratados.rds")
+tab_en <- readr::read_rds("./data-raw/rds/en/dados_agregados_1_1_1_tratados.rds") |>
+  dplyr::select(variavel_en = variavel)
+tab <- cbind(tab, tab_en)
+DBI::dbWriteTable(con, "grafico_variacao_consumo_setorial_el", tab, overwrite = TRUE)
+
+# grafico_crescimento_consumo_eletricidade_industria
+tab <- readr::read_rds("./data-raw/rds/pt/dados_agregados_1_1_2_tratados.rds")
+tab_en <- readr::read_rds("./data-raw/rds/en/dados_agregados_1_1_2_tratados.rds") |>
+  dplyr::select(variavel_en = variavel)
+tab <- cbind(tab, tab_en)
+DBI::dbWriteTable(con, "grafico_crescimento_consumo_eletricidade_industria", tab, overwrite = TRUE)
+
+# grafico_participacao_setorial_consumo_el
+tab <- readr::read_rds("./data-raw/rds/pt/dados_agregados_1_1_3_tratados.rds")
+tab_en <- readr::read_rds("./data-raw/rds/en/dados_agregados_1_1_3_tratados.rds") |>
+  dplyr::select(variavel_en = variavel)
+tab <- cbind(tab, tab_en)
+DBI::dbWriteTable(con, "grafico_participacao_setorial_consumo_el", tab, overwrite = TRUE)
+
+# grafico_participacao_fontes_capacidade_instalada
+tab <- readr::read_rds("./data-raw/rds/pt/dados_agregados_1_1_4_tratados.rds")
+tab_en <- readr::read_rds("./data-raw/rds/en/dados_agregados_1_1_4_tratados.rds") |>
+  dplyr::select(variavel_en = variavel)
+tab <- cbind(tab, tab_en)
+DBI::dbWriteTable(con, "grafico_participacao_fontes_capacidade_instalada", tab, overwrite = TRUE)
+
+
+DBI::dbDisconnect(con)
