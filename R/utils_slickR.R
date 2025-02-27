70fa2e76beb875370dac0d9feba2e649d1546d3d
williamorim <thewilliam89@gmail.com>
2025-02-27 13:56:55 -0300

    graficos de linha e area

---
+++ R/utils_slickR.R
@@ -0,0 +1,41 @@
+transformar_svg <- function(p) {
+  svglite::xmlSVG(show(p), standalone = TRUE, width = 8, height = 4)
+}
+
+#' Gerar carrossel com gráficos
+#'
+#' Esta função cria um carrossel de gráficos utilizando a biblioteca `slickR`.
+#'
+#' @param ... Gráficos a serem incluídos no carrossel.
+#' @param labels Vetor de caracteres com os rótulos a serem exibidos nos pontos do carrossel. O padrão é c("Área empilhada", "Linha").
+#'
+#' @return Um objeto `slickR` que representa o carrossel de gráficos.
+#'
+#' @examples
+#' \dontrun{
+#'   p1 <- ggplot2::qplot(mpg, wt, data = mtcars)
+#'   p2 <- ggplot2::qplot(hp, wt, data = mtcars)
+#'   gerar_carrossel(p1, p2)
+#' }
+#'
+#' @export
+criar_carrossel <- function(..., labels = c("Área empilhada", "Linha")) {
+  lista <- purrr::map(list(...), transformar_svg)
+
+  dot_labels <- htmlwidgets::JS(
+    glue::glue(
+      "function(slick, index) {
+        if (index == 0) {
+          return '{{labels[1]}}';
+        } else if (index == 1) {
+          return '{{labels[2]}}';
+        }
+       }",
+      .open = "{{",
+      .close = "}}"
+    )
+  )
+
+  slickR::slickR(lista, width = "90%", height = 550) +
+    slickR::settings(dots = TRUE, customPaging = dot_labels)
+}
