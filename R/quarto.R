2238e4cdf45e6f83c3eb420ba8e58702cfcbc52f
williamorim <thewilliam89@gmail.com>
2025-06-03 15:32:38 -0300

    colocando capa

--- R/quarto.R
+++ R/quarto.R
@@ -1,22 +1,27 @@
 gerar_livro <- function(con = NULL, ano = 2023,
                         out_dir = "~/Desktop/ben_corrido",
+                        profiles = c("pt", "en"),
                         duckdb_path = "~/Documents/curso-r/ben_corrido/data-raw/bd_ouro.duckdb") {
   fs::dir_copy(
     path = sys_file("quarto"),
     new_path = glue::glue("{out_dir}/quarto"),
     overwrite = TRUE
   )
-  quarto::quarto_render(
-    input = glue::glue("{out_dir}/quarto"),
-    as_job = FALSE,
-    execute_params = list(ano = ano, duckdb_path = duckdb_path),
-    profile = "pt"
-  )
-  quarto::quarto_render(
-    input = glue::glue("{out_dir}/quarto"),
-    as_job = FALSE,
-    execute_params = list(ano = ano, duckdb_path = duckdb_path),
-    profile = "en"
-  )
+  if ("pt" %in% profiles) {
+    quarto::quarto_render(
+      input = glue::glue("{out_dir}/quarto"),
+      as_job = FALSE,
+      execute_params = list(ano = ano, duckdb_path = duckdb_path),
+      profile = "pt"
+    )
+  }
+  if ("en" %in% profiles) {
+    quarto::quarto_render(
+      input = glue::glue("{out_dir}/quarto"),
+      as_job = FALSE,
+      execute_params = list(ano = ano, duckdb_path = duckdb_path),
+      profile = "en"
+    )
+  }
   fs::dir_delete(glue::glue("{out_dir}/quarto"))
 }
