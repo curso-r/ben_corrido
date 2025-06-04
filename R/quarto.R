3d99061952ddbca8d0e690a6a7d9e48d0df047ff
williamorim <thewilliam89@gmail.com>
2025-06-04 12:52:44 -0300

    documentando funcao

--- R/quarto.R
+++ R/quarto.R
@@ -1,5 +1,20 @@
-gerar_livro <- function(con = NULL, ano = 2023,
-                        out_dir = "~/Desktop/ben_corrido",
+#' Gera o livro em diferentes perfis de idioma usando Quarto
+#'
+#' Esta função copia o diretório de arquivos Quarto, renderiza o livro nos perfis especificados (por exemplo, português e inglês)
+#' e remove o diretório temporário após a renderização.
+#'
+#' @param out_dir Diretório de saída onde o livro será gerado. Padrão é \code{"~/Desktop/ben_corrido"}.
+#' @param profiles Vetor de perfis de idioma a serem renderizados. Padrão é \code{c("pt", "en")}.
+#' @param duckdb_path Caminho para o banco de dados DuckDB utilizado na renderização. Padrão é \code{"~/Documents/curso-r/ben_corrido/data-raw/bd_ouro.duckdb"}.
+#'
+#' @return Não retorna valor. Gera os arquivos do livro nos perfis especificados.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' gerar_livro(profiles = c("pt"))
+#' }
+gerar_livro <- function(out_dir = "~/Desktop/ben_corrido",
                         profiles = c("pt", "en"),
                         duckdb_path = "~/Documents/curso-r/ben_corrido/data-raw/bd_ouro.duckdb") {
   fs::dir_copy(
@@ -11,7 +26,7 @@ gerar_livro <- function(con = NULL, ano = 2023,
     quarto::quarto_render(
       input = glue::glue("{out_dir}/quarto"),
       as_job = FALSE,
-      execute_params = list(ano = ano, duckdb_path = duckdb_path),
+      execute_params = list(duckdb_path = duckdb_path),
       profile = "pt"
     )
   }
@@ -19,7 +34,7 @@ gerar_livro <- function(con = NULL, ano = 2023,
     quarto::quarto_render(
       input = glue::glue("{out_dir}/quarto"),
       as_job = FALSE,
-      execute_params = list(ano = ano, duckdb_path = duckdb_path),
+      execute_params = list(duckdb_path = duckdb_path),
       profile = "en"
     )
   }
